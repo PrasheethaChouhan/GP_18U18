7ef80709f659ec0d73974557b7c79ebbdd9d0714
the Prasheetha <60392154+PrasheethaChouhan@users.noreply.github.com>
2021-09-30 18:24:30 +0530

    Add files via upload

---
+++ 18U18_GP_NGO/components/AppStackNavigator.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+
+import CreateEventScreen from "../screens/CreateEventScreen";
+import Home from "../screens/Home";
+import NGOeventDetail from "../screens/NGOeventDetail";
+import GetDonation from "../screens/GetDonation";
+import QRCode from "../screens/QRCode-Scan";
+
+export const AppStackNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+    CreateEventScreen: {
+      screen: CreateEventScreen,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+    NGOeventDetail: {
+      screen: NGOeventDetail,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+    GetDonation: {
+      screen: GetDonation,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+
+    QRCode: {
+      screen: QRCode,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+  },
+  {
+    initialRouteName: "Home",
+  }
+);
